d1b9b649b4f21aca697b35d67a6da565f7f8cbeb
Sharathxct <sharathxct@gmail.com>
2025-03-14 22:55:46 +0530

    test

--- apps/goalfi/src/navigation/index.tsx
+++ apps/goalfi/src/navigation/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from "../screens/Home"
+import { usePrivy } from '@privy-io/expo';
 
 type RootStackParamList = {
   Home: undefined;
@@ -10,6 +11,8 @@ type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const AppNavigation = () => {
+  const { user, isReady } = usePrivy();
+  console.log(user);
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }} >
