99a58d0f552ed44cfce616c2660b9bd96120d4e3
Sharathxct <sharathxct@gmail.com>
2025-03-14 12:50:23 +0530

    migrating to react native 🥲

---
+++ apps/goalfi/App.tsx
@@ -0,0 +1,17 @@
+import './global.css'
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export default App;
--- apps/goalfi/tailwind.config.js
+++ apps/goalfi/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./App.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
   plugins: [],
-  presets: [require("nativewind/preset")],
-};
+}
