bd7acd279c81d97cb8cb4e9e463eba0a7789bbb6
Sharathxct <sharathxct@gmail.com>
2025-03-14 22:46:20 +0530

    Privy integration done

--- apps/goalfi/App.tsx
+++ apps/goalfi/App.tsx
@@ -1,11 +1,20 @@
 import './global.css'
 import React from 'react';
 import AppNavigation from './src/navigation';
+import 'fast-text-encoding';
+import 'react-native-get-random-values';
+import '@ethersproject/shims';
+import {Buffer} from 'buffer';
+import { PrivyProvider } from '@privy-io/expo';
+
+global.Buffer = Buffer;
 
 function App(): React.JSX.Element {
 
   return (
-    <AppNavigation />
+    <PrivyProvider appId='cm83cua1o00jz7u2d8tkzmsqu' clientId='client-WY5hqT4XnBnZmk5uZzMDAv7VjLDac9sJLFPDDU74ryka2'>
+      <AppNavigation />
+    </PrivyProvider>
   );
 }
 
--- apps/goalfi/src/screens/Home.tsx
+++ apps/goalfi/src/screens/Home.tsx
@@ -1,9 +1,23 @@
-import { Text, View } from "react-native";
-
+import { Text, TouchableOpacity, View } from "react-native";
+import { useLoginWithEmail } from "@privy-io/expo";
 function Home() {
+  const {sendCode} = useLoginWithEmail();
+  const handleLogin = async () => {
+    try {
+      const res = await sendCode({
+        email: 'sharathxct@gmail.com',
+      });
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <View className="flex-1 items-center justify-center bg-white font-bold text-2xl">
-      <Text>Home</Text>
+      <Text>Home screen</Text>
+      <TouchableOpacity onPress={handleLogin}>
+        <Text>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
