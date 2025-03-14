750f08d038aae96f06ea5f519c7f8d88678a6f25
Sharathxct <sharathxct@gmail.com>
2025-03-14 13:51:23 +0530

    navigation fix

--- apps/goalfi/App.tsx
+++ apps/goalfi/App.tsx
@@ -1,6 +1,7 @@
 import './global.css'
 import React from 'react';
 import AppNavigation from './src/navigation';
+import { View, Text } from 'react-native';
 
 function App(): React.JSX.Element {
 
--- apps/goalfi/src/screens/Home.tsx
+++ apps/goalfi/src/screens/Home.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 function Home() {
   return (
-    <View>
+    <View className="flex-1 items-center justify-center bg-white font-bold text-2xl">
       <Text>Home</Text>
     </View>
   );
