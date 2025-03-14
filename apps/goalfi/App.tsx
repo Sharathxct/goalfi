f54adae283af335390802ffdf9f481dfd555414b
Sharathxct <sharathxct@gmail.com>
2025-03-14 13:24:36 +0530

    navigation

--- apps/goalfi/App.tsx
+++ apps/goalfi/App.tsx
@@ -1,16 +1,11 @@
 import './global.css'
 import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import AppNavigation from './src/navigation';
 
 function App(): React.JSX.Element {
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Hello World</Text>
-    </View>
+    <AppNavigation />
   );
 }
 
---
+++ apps/goalfi/src/navigation/index.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from "../screens/Home"
+
+type RootStackParamList = {
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const AppNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }} >
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigation;
